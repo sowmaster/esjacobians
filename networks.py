b57cbc02814065187164c30a05c74144cff71c90
Daouda SOW <bookAir@MacBook-Pro.local>
2021-10-20 02:09:10 -0400

    initial commit

---
+++ networks.py
@@ -0,0 +1,390 @@
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def OmniglotNetFeats(hidden_size):
+    def conv_layer(ic, oc, ):
+        return nn.Sequential(
+            nn.Conv2d(ic, oc, 3, padding=1), nn.ReLU(inplace=True), nn.MaxPool2d(2),
+            nn.BatchNorm2d(oc, momentum=1., affine=True,
+                           track_running_stats=True
+                           )
+        )
+
+    net =  nn.Sequential(
+        conv_layer(1, hidden_size),
+        conv_layer(hidden_size, hidden_size),
+        conv_layer(hidden_size, hidden_size),
+        conv_layer(hidden_size, hidden_size),
+        nn.Flatten())
+
+    #initialize(net)
+    return net
+
+
+def MiniimageNetFeats(hidden_size):
+    def conv_layer(ic, oc):
+        return nn.Sequential(
+            nn.Conv2d(ic, oc, 3, padding=1), nn.ReLU(inplace=True), nn.MaxPool2d(2),
+            nn.BatchNorm2d(oc, momentum=1., affine=True,
+                           track_running_stats=False
+                           )
+        )
+
+    net = nn.Sequential(
+        conv_layer(3, hidden_size),
+        conv_layer(hidden_size, hidden_size),
+        conv_layer(hidden_size, hidden_size),
+        conv_layer(hidden_size, hidden_size),
+        nn.Flatten())
+
+    #initialize(net)
+    return net
+
+
+# def initialize(net):
+#     # initialize weights properly
+#     for m in net.modules():
+#         if isinstance(m, nn.Conv2d):
+#             n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+#             m.weight.data.normal_(0, math.sqrt(2. / n))
+#             if m.bias is not None:
+#                 m.bias.data.zero_()
+#         elif isinstance(m, nn.BatchNorm2d):
+#             m.weight.data.fill_(1)
+#             m.bias.data.zero_()
+#         elif isinstance(m, nn.Linear):
+#             #m.weight.data.normal_(0, 0.01)
+#             #m.bias.data = torch.ones(m.bias.data.size())
+#             m.weight.data.zero_()
+#             m.bias.data.zero_()
+#
+#     return net
+
+
+
+class classifier(nn.Module):
+
+    def __init__(self, n_features=84, n_classes=10):
+        super(classifier, self).__init__()
+
+        self.fc = nn.Linear(n_features, n_classes)
+
+    def forward(self, feats):
+        output = self.fc(feats)
+
+        return output
+
+########### LeNet network ##############
+class LeNet5Feats(nn.Module):
+
+    def __init__(self):
+        super(LeNet5Feats, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=(5, 5))
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=(5, 5))
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv3 = nn.Conv2d(16, 120, kernel_size=(5, 5))
+        self.relu3 = nn.ReLU()
+        self.fc1 = nn.Linear(120, 84)
+        self.relu4 = nn.ReLU()
+        #self.fc2 = nn.Linear(84, 10)
+
+    def forward(self, img, out_feature=False):
+        output = self.conv1(img)
+        output = self.relu1(output)
+        output = self.maxpool1(output)
+        output = self.conv2(output)
+        output = self.relu2(output)
+        output = self.maxpool2(output)
+        output = self.conv3(output)
+        output = self.relu3(output)
+        output = output.view(-1, 120)
+        output = self.fc1(output)
+        output = self.relu4(output)
+        #output = self.fc2(output)
+
+        return output
+
+
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1   = nn.Linear(16*5*5, 120)
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, 10)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
+
+class LeNet5(nn.Module):
+
+    def __init__(self):
+        super(LeNet5, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=(5, 5))
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=(5, 5))
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv3 = nn.Conv2d(16, 120, kernel_size=(5, 5))
+        self.relu3 = nn.ReLU()
+        self.fc1 = nn.Linear(120, 84)
+        self.relu4 = nn.ReLU()
+        self.fc2 = nn.Linear(84, 10)
+
+    def forward(self, img, out_feature=False):
+        output = self.conv1(img)
+        output = self.relu1(output)
+        output = self.maxpool1(output)
+        output = self.conv2(output)
+        output = self.relu2(output)
+        output = self.maxpool2(output)
+        output = self.conv3(output)
+        output = self.relu3(output)
+        feature = output.view(-1, 120)
+        output = self.fc1(feature)
+        output = self.relu4(output)
+        output = self.fc2(output)
+        if out_feature == False:
+            return output
+        else:
+            return output, feature
+
+
+class LeNet5Half(nn.Module):
+
+    def __init__(self):
+        super(LeNet5Half, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 3, kernel_size=(5, 5))
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv2 = nn.Conv2d(3, 8, kernel_size=(5, 5))
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv3 = nn.Conv2d(8, 60, kernel_size=(5, 5))
+        self.relu3 = nn.ReLU()
+        self.fc1 = nn.Linear(60, 42)
+        self.relu4 = nn.ReLU()
+        self.fc2 = nn.Linear(42, 10)
+
+    def forward(self, img, out_feature=False):
+        output = self.conv1(img)
+        output = self.relu1(output)
+        output = self.maxpool1(output)
+        output = self.conv2(output)
+        output = self.relu2(output)
+        output = self.maxpool2(output)
+        output = self.conv3(output)
+        output = self.relu3(output)
+        feature = output.view(-1, 60)
+        output = self.fc1(feature)
+        output = self.relu4(output)
+        output = self.fc2(output)
+        if out_feature == False:
+            return output
+        else:
+            return output, feature
+
+
+############ VGG networks #################
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG(nn.Module):
+    def __init__(self, vgg_name):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+
+
+########### Residual networks ################
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+class ResNetFeats(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNetFeats, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        # self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        # out = self.linear(out)
+        return out
+
+def ResNetFeats18():
+    return ResNetFeats(BasicBlock, [2, 2, 2, 2])
+
+def ResNet18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
+
+
+def ResNet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+
+def ResNet50():
+    return ResNet(Bottleneck, [3, 4, 6, 3])
+
+
+def ResNet101():
+    return ResNet(Bottleneck, [3, 4, 23, 3])
+
+
+def ResNet152():
+    return ResNet(Bottleneck, [3, 8, 36, 3])
